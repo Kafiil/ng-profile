0320ffca82e23d003bd722247a6490aab3cc32da
Kafil <kafil.nasdami@gmail.com>
2017-09-25 03:25:14 +0200

    Implementing a scrollspy

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,15 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
   year: number;
+  author:string;
   constructor() { }
 
   ngOnInit() {
     const today = new Date();
     this.year = today.getFullYear();
+
+    // TODO: Get this info from local json file
+    this.author = "NASDAMI Kafil";
   }
 
 }

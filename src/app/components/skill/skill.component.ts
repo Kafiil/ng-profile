a9065f8cc57cd932000a95d3a38adc6285614a96
Kafil <kafil.nasdami@gmail.com>
2017-09-29 04:58:14 +0200

    Styling the progress bar

--- src/app/components/skill/skill.component.ts
+++ src/app/components/skill/skill.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-skill',
   templateUrl: './skill.component.html',
-  styleUrls: ['./skill.component.css']
+  styleUrls: ['./skill.component.scss']
 })
 export class SkillComponent implements OnInit {
   @Input('skill') skill: Skill;

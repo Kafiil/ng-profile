555d2f32ae670a7526c5723bfc274a253fd7cadf
Kafil <kafil.nasdami@gmail.com>
2017-10-06 17:56:36 +0100

    fixinig the model bug

--- src/app/models/info.ts
+++ src/app/models/info.ts
@@ -1,7 +1,14 @@
 export class Info {
   name: string;
+  jobTitle: string;
   dateOfBirth: string;
   country: string;
   city: string;
   aboutMe: string;
+  favoriteQuote: Quote;
+}
+
+export class Quote {
+  content: string;
+  author: string;
 }

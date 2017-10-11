edef3566fde4960200b71807cdf7458fd8199ef6
Kafil <kafil.nasdami@gmail.com>
2017-10-11 02:00:06 +0100

    fixing the study section styles

--- src/app/components/studies/studies.component.ts
+++ src/app/components/studies/studies.component.ts
@@ -15,9 +15,9 @@ export class StudiesComponent implements OnInit {
 
   ngOnInit() {
     this.dataService.getStudies()
-      // .do((res: Study[]) => {
-      //   res.sort((a, b) => Study.orderStudyDesc(a.period, b.period));
-      // })
+      .do((res: Study[]) => {
+        res.sort((a, b) => Study.orderStudyDesc(a.period, b.period));
+      })
       .subscribe(v => {
         this.studies = v;
       });
